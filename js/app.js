ed6064d3c6bde13d1ce0fbdd83a825ea93a07c3d
Karol Krusiec <karol@krusiec.com>
2017-11-22 15:17:39 +0100

    Added switching between simple and complicated structure

--- js/app.js
+++ js/app.js
@@ -1,22 +1,40 @@
 (function () {
   const form = document.querySelector('form');
-  const input = form.querySelectorAll('input');
-  let structure = {};
-  let key;
-  let val;
+  const treeView = document.querySelector('.tree-view');
+  const structure = document.querySelectorAll('input[name="structure"]');
+  const simpleSet = document.querySelectorAll('.simple-set');
+  const complicatedSet = document.querySelectorAll('.complicated-set');
+  let itsComplicated = false;
+  let newUl = document.createElement('ul');
+  let newLi = document.createElement('li');
+  let mainFolder = treeView.children[0].children[0];
+  const element = '<ul class="complicated-folder"><li>src</li><li>dist</li></ul>';
 
-  for (var i = 0; i < input.length - 1; i++) {
-    key = input[i].name + '-' + input[i].value;
-    val = input[i].checked;
+  for (let i = 0, len = structure.length; i < len; i++) {
+    structure[i].addEventListener('change', function () {
+      if (this.value.toUpperCase() === 'COMPLICATED') {
+        for (let j = 0, setLength = simpleSet.length; j < setLength; j++) {
+          simpleSet[j].classList.add('hidden');
+        }
+        for (let j = 0, setLength = complicatedSet.length; j < setLength; j++) {
+          complicatedSet[j].classList.remove('hidden');
+        }
 
-    structure[key] = val;
+        mainFolder.insertAdjacentHTML('beforeend', element);
 
-    input[i].addEventListener('change', function () {
-      console.log(key + " " + this.checked);
-      console.log(this.type);
+        itsComplicated = true;
+      } else {
+        const complicatedFolder = document.querySelector('.complicated-folder');
+        for (let j = 0, setLength = simpleSet.length; j < setLength; j++) {
+          simpleSet[j].classList.remove('hidden');
+        }
+        for (let j = 0, setLength = complicatedSet.length; j < setLength; j++) {
+          complicatedSet[j].classList.add('hidden');
+        }
+        itsComplicated = false;
+      }
     })
-  }
 
-  console.log(structure);
+  }
 
 })();
